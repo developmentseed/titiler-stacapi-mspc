a2b3376fbf590d3227966b61cf31b2c3392a0fd2
Aimee Barciauskas <aimee@developmentseed.org>
2024-06-03 16:33:10 -0700

    working on tile test

--- tests/test_stac_reader.py
+++ tests/test_stac_reader.py
@@ -3,13 +3,21 @@
 import json
 import os
 
-import pystac
+from .conftest import mock_rasterio_open
+
 import pytest
+from unittest.mock import patch
 from rio_tiler.io import Reader
+from rio_tiler.models import ImageData
 
 from titiler.stacapi.stac_reader import CustomSTACReader
 from titiler.stacapi.xarray import XarrayReader
 from titiler.stacapi.models import AssetInfo
+
+item_file = os.path.join(
+    os.path.dirname(__file__), "fixtures", "20200307aC0853900w361030.json"
+)
+item_json = json.loads(open(item_file).read())
 
 @pytest.mark.skip(reason="To be implemented.")
 def test_asset_info():
@@ -17,16 +25,15 @@ def test_asset_info():
     pass
 
 empty_stac_reader = CustomSTACReader({'assets': [], 'bbox': []})
-def test_stac_reader_cog():
+def test_get_reader_cog():
     """Test reader is rio_tiler.io.Reader"""
     asset_info = AssetInfo(
-        url="https://file.tif",
-        type="image/tiff"
+        url="https://file.tif"
     )
     assert empty_stac_reader._get_reader(asset_info) == Reader
 
 
-def test_stac_reader_netcdf():
+def test_get_reader_netcdf():
     """Test reader attribute is titiler.stacapi.XarrayReader"""
     asset_info = AssetInfo(
         url="https://file.nc",
@@ -34,8 +41,13 @@ def test_stac_reader_netcdf():
     )
     assert empty_stac_reader._get_reader(asset_info) == XarrayReader
 
-def test_tile_cog():
-    pass
+@patch("rio_tiler.io.rasterio.rasterio")
+def test_tile_cog(rio):
+    rio.open = mock_rasterio_open
+    with CustomSTACReader(item_json) as reader:
+        img = reader.tile(0, 0, 0, assets=['cog'])
+        assert type(img) == ImageData
 
+@pytest.mark.skip(reason="To be implemented.")
 def test_tile_netcdf():
     pass
